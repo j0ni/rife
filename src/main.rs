cfed9c23678f61f02d25c5c75b24aa56f79cb74e
J Irving <j@lollyshouse.ca>
2018-09-29 17:37:07 +0100

    Load coords from JSON file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate piston_window;
+extern crate serde_json;
 
 mod board;
 mod draw;
@@ -7,16 +8,39 @@ mod game;
 use piston_window::types::Color;
 use piston_window::{clear, PistonWindow, UpdateEvent, WindowSettings};
 
+use serde_json::Value;
+use std::fs;
+
 use self::draw::to_coord_u32;
 use self::game::Game;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
+fn load_coords(fname: String) -> Vec<(i32, i32)> {
+    let text = fs::read_to_string(fname).unwrap();
+    let v: Value = serde_json::from_str(&text).unwrap();
+    let pairs: &Vec<Value> = v.as_array().unwrap();
+    let mut coords: Vec<(i32, i32)> = Vec::new();
+
+    for pair in pairs {
+        let pair = pair.as_array().unwrap();
+        assert_eq!(pair.len(), 2);
+
+        coords.push((
+            pair[0].as_i64().unwrap() as i32,
+            pair[1].as_i64().unwrap() as i32,
+        ));
+    }
+
+    coords
+}
+
 fn main() {
     let (width, height) = (50, 50);
 
     let mut game = Game::new(width, height);
-    game.init(&vec![(2, 3), (3, 4), (4, 4), (4, 3), (4, 2)]);
+    let coords = load_coords(String::from("coords.json"));
+    game.init(&coords);
 
     let mut window: PistonWindow =
         WindowSettings::new("Rrrrrife!", [to_coord_u32(width), to_coord_u32(height)])
